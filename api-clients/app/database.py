b9df6587c19152c8cf814d1dd4119bf1929c5b38
RokibEPSI <rokibou.sikirou@ecoles-epsi.net>
2025-07-08 02:33:49 +0200

    update ci

--- api-clients/app/database.py
+++ api-clients/app/database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base  
+from dotenv import load_dotenv
 import os
-#from dotenv import load_dotenv
-#load_dotenv()
-# Modifier  PostgreSQL
-#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:rkb.0102@localhost:5432/paye_ton_kawa")
+
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# ✅ Initialisation SQLAlchemy
 engine = create_engine(DATABASE_URL)
-#engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
